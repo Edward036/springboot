831d22cc02b8325c6c16db7cd37a10c190ad6b68
mrmeisen <ms915818993@163.com>
2017-04-20 20:56:05 +0800

    build a mvc project

--- src/main/java/org/springboot/ctl/helloController.java
+++ src/main/java/org/springboot/ctl/helloController.java
@@ -1,17 +1,20 @@
 package org.springboot.ctl;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * @author meisen
  * 2017-04-18
  */
-@RestController
+@Controller
 public class helloController {
 
-    @RequestMapping("/")
-    public String index(){
+    @RequestMapping("/index")
+    public String index(@RequestParam(value = "name", required = false, defaultValue = "World") String name, Model model) {
+        model.addAttribute("name", name);
         return "index";
     }
 }
