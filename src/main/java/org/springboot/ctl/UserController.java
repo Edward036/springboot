0ab0ed898a343f9251be00e93a6c760a74b69dac
mrmeisen <ms915818993@163.com>
2017-05-02 22:14:43 +0800

    添加resultful代码,测试未完成

---
+++ src/main/java/org/springboot/ctl/UserController.java
@@ -0,0 +1,53 @@
+package org.springboot.ctl;
+
+import org.springboot.entity.User;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.*;
+
+/**
+ * @author meisen
+ * 2017-05-02
+ */
+@RestController
+@RequestMapping("/users")
+public class UserController {
+
+    //创建线程安装的Map
+    private static Map<Long, User> users = Collections.synchronizedMap(new HashMap<>());
+
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public List<User> getUserList() {
+        // 可以通过@RequestParam从页面中传递参数来进行查询条件或者翻页信息的传递
+        return new ArrayList<>(users.values());
+    }
+
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    public String createAUser(@ModelAttribute User user) {
+        users.put(user.getId(), user);
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.GET)
+    public User getUser(@PathVariable Long id) {
+        return users.get(id);
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.POST)
+    public String updateUser(@ModelAttribute User user, @PathVariable Long id) {
+        User u = users.get(id);
+        u.setAge(user.getAge());
+        u.setId(user.getId());
+        return "success";
+    }
+
+    @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
+    public String deleteUser(@PathVariable Long id){
+        users.remove(id);
+        return "success";
+    }
+
+}
+
+
+
---
+++ src/test/java/org/springboot/UserTests.java
@@ -0,0 +1,49 @@
+package org.springboot;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springboot.ctl.UserController;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+
+
+
+/**
+ * @author meisen
+ *         2017-05-02
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest
+@WebAppConfiguration
+public class UserTests {
+
+    private MockMvc mvc;
+
+    @Before
+    public void setUp() throws Exception {
+        mvc = MockMvcBuilders.standaloneSetup(new UserController()).build();
+    }
+
+    @Test
+    public void userCtlTest() throws Exception {
+
+        RequestBuilder request = null;
+
+        //提取user列表
+        request = get("/users/");
+        mvc.perform(request)
+                .andExpect(status().isOk())
+                .andExpect(content().string(equalTo("[]")));
+
+    }
+}
