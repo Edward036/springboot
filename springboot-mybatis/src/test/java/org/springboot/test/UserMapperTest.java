8eb95eec318a312d3e4bf9abf54085e84369b88f
smei <smei@travelsky.com>
2017-06-09 17:07:52 +0800

    构建mybatis单元测试

--- springboot-mybatis/src/test/java/org/springboot/test/UserMapperTest.java
+++ springboot-mybatis/src/test/java/org/springboot/test/UserMapperTest.java
@@ -1,23 +1,25 @@
 package org.springboot.test;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springboot.Application;
 import org.springboot.mapper.UserMapper;
+import org.springboot.modal.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 /**
  * @author meisen
  * 2017-06-08
  */
 @RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = UserMapper.class)
+@SpringBootTest(classes = Application.class)
 @Transactional
-@ComponentScan(basePackages={"org.springboot.mapper"})
-
 public class UserMapperTest {
 
     @Autowired
@@ -25,6 +27,7 @@ public class UserMapperTest {
 
     @Test
     public void testGetAllUser(){
-        System.out.println(userMapper.getAllUser());
+        List<User> users = userMapper.getAllUser();
+        Assert.assertEquals("Tom", users.get(0).getName());
     }
 }
