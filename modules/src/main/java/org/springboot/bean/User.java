255b4d771672a89971f5b40cf63e2112b1012262
mrmeisen <ms915818993@163.com>
2017-12-22 18:17:42 +0800

    add

---
+++ modules/src/main/java/org/springboot/bean/User.java
@@ -0,0 +1,48 @@
+package org.springboot.bean;
+
+
+/**
+ * @author meisen
+ * 2017-12-22
+ */
+public class User {
+
+    private Long id;
+
+    private String name;
+
+    private Integer age;
+
+    public User() {
+    }
+
+    public User(Long id, String name, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
